47248461fc1d590c7ca1fbcd52e0950c5066ab4c
P15241328 <P15241328@myemail.dmu.ac.uk>
2021-03-23 17:52:27 +0100

    commited to Github

--- README.md
+++ README.md
@@ -4,4 +4,74 @@ Assignment 2 from the course LT2222 in the University of Gothenburg's winter 202
 
 Your name: Mark Grech
 
-*Answer all questions in the notebook here.  You should also write whatever high-level documentation you feel you need to here.*
+The aim of this document is to briefly explain the inner workings and the methodology employed behind the development of assessment no. 2.
+
+**Part 1**
+The first part of the assignment entails the loading of the GMB data file. Loading of file simply occurs through the parsing of each row.
+Each word is lemmatised to ensure the corpus is standardised as much as possible. In addition, words that are deemed
+to be of no relevance or put differently, of little predictive power to NE class are excluded. 
+The removal of such words resulted in the reduction of observations/rows by 34%. 
+
+**Part 2**
+The second part of the assignment involves the implementation of the core functionality as it entails the construction of sentences present within the text file and the identification of words that are deemed to be key Named Entities. Each observation is tagged with a sentence number which, when considered in sequential order of loading, the group of words constitute to a single sentence.
+The logic behind the transformation of observations stored in the original dataframe to class instances and back to a dataframe is performed by the gmb_processor class. The class implements the following methods/functions:
+
+**Entity**
+Class which stores the properties associated with each word namely tag, POS (part-of-speech), position of word in the entire sentence as well as other helper properties (eg. isFirst, isLast,isNE). 
+
+**gmb_processor**
+1. __aggregator_function(). A row iterator function that removes punctuation, constructs each sentence and creates an instance of class Entity.     
+2. __aggregate(). Groups observations by Sentence# and applies the aggregator function to the entire dataset.
+3. fit(). Converts the aggregated dataset to a list of class instances.
+4. get_instances(n,pad,skip_ne). The main function which instigates the transformation process and returns a dataset having Named Entity Classes and the n surrounding words that happen to occur on the left/right hand side of the NE.
+                                 n - Determines the number of words that should be considered before and after the NE. If n=5 then the return dataset consists of (x classes/rows, n*2 words/columns )   
+                                 pad - Whether padding should be applied in order to retain the position of words in relation to the beginning and end of the sentence. 
+                                 skip_ne - Determines whether words that happen to be NE should be removed from the peripheral words.
+   
+
+**Part 3**
+Responsible for the vectorisation of class instances. This implies the conversion of class and the associated words to vectors through the application of BOW or TFIDF techniques.
+The target feature named _class_ represents the NE which is of categorical data type and consists of 8 levels namely   
+'art', 'eve', 'geo', 'gpe', 'nat', 'org', 'per', 'tim'. A description of each class follows:
+geo = Geographical Entity
+org = Organization
+per = Person
+gpe = Geopolitical Entity
+tim = Time indicator
+art = Artifact
+eve = Event
+nat = Natural Phenomenon
+       
+The second part partitions the dataset to the typical train/test subsets with an 80:20 ratio using the random holdout method (no bootstapping). No stratification sampling has been applied. 
+
+**Part 4**
+The training of the Support Vector based model occurs and the model's performance is evaluated on both the train and test subsets.
+
+**Part 5**
+The predictive performance of the SVM data model is evaluated against the accuracy metric. It has been observed that high accuracy scores are yielded on the training dataset (0.9) whilst lower scores (0.5) are achieved on the test dataset. These figures suggest model overfitting whereby the SVM model has learnt well the associations between the words and the respective NE classes. However, the model is unable to generalise well on unseen/test data. As a matter of fact the 50% accuracy score on test data suggests that the model is as good as a random guess.
+
+The normalised confusion matrix portrays low True Positive (TP) rates as well as severe multi-class misclassifications.
+By way of example, only 28% and 38% the GPE and NAT classes respectively were properly classified.
+The EVE Named Entity achieved the highest misclassification score, most of which (32%) where incorrectly classified as a ORG Named Entity class.
+
+Given that the data modelling phases is typically carried out iteratively, the following actions were taken in attempt to improve the accuracy of the linear SVM model.
+1. Increase the number of boundary words with n=10, but resulted in a sparser dataset. Part 2.
+2. Inclusion of other NE in boundary words during the building of class instances. Part 2.
+3. The removal of padding using surrogate words such as S1,S2,S3,S4,S5. Part 2.
+4. The inclusion of TFIDF instead of BOW vectorisation method. Part 3.
+5. Evaluating the performance of other ML algorithms through the use of LazyClassifer library.
+
+**Bonus Part A**
+Through basic exploratory data analysis it is clear that the Named Entity dataset is highly imbalanced.
+The GEO class is represented by 25% of the class instances whilst the NAT class is highly under-represented (only 0.3% of the instances) 
+It is to be expected that such a highly imbalanced dataset result in misclassification of the under-represented classes.
+
+NE Class Percentage of class instances 
+geo      25.51%
+per      23.02%
+org      20.87%
+tim      15.24%
+gpe      13.32%
+eve      0.87%
+art      0.86%
+nat      0.31%
--- a2.py
+++ a2.py
@@ -4,6 +4,14 @@ import numpy as np
 import numpy.random as npr
 import pandas as pd
 import random
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,plot_confusion_matrix
+import scikitplot as skplt
 
 # Module file with functions that you fill in so that they can be
 # called by the notebook.  This file should be in the same
@@ -14,56 +22,264 @@ import random
 # result (though not necessarily a good classifier).  Document your
 # code with reasonable comments.
 
+
+
 # Function for Part 1
+#1. To preprocess the text (lowercase and lemmatize; punctuation can be preserved as it gets its own rows).
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+        self.stop_words = set(stopwords.words('english'))
+
+    def lemmatize(self, text, remove_stop=True):
+        if remove_stop and (text in self.stop_words):
+            return np.nan
+        else:
+            return self.lemmatizer.lemmatize(text)
+
+
 def preprocess(inputfile):
-    return inputfile.readlines()
+    def parse_line(line):
+        file_line = (line.split('\t'))
+        file_line[len(file_line) - 1] = file_line[len(file_line) - 1].split('\n')[0]
+        return file_line[1:]  # ignore line sequence id
 
-# Code for part 2
-class Instance:
-    def __init__(self, neclass, features):
-        self.neclass = neclass
-        self.features = features
+    # df = pd.read_csv('GMB_dataset.txt',sep='\t', lineterminator='\r',header=0)
 
-    def __str__(self):
-        return "Class: {} Features: {}".format(self.neclass, self.features)
+    # read text from file
+    lines = inputfile.readlines()  # read all lines
+    if (inputfile is not None):
+        inputfile.close()
 
-    def __repr__(self):
-        return str(self)
+    # Parse file rows
+    num_rows = len(lines) - 1
+    header = (parse_line(lines[0]))
+    parse_lines = [parse_line(line) for line in lines[1:]]
 
-def create_instances(data):
-    instances = []
-    for i in range(100):
-        neclass = random.choice(['art','eve','geo','gpe','nat','org','per','tim'])
-        features = ["something"] * random.randint(1, 10)
-        instances.append(Instance(neclass, features))
-    return instances
+    # Convert to a dataframe
+    df = pd.DataFrame(parse_lines, columns=header)
+    df['Word'] = df['Word'].apply(lambda x: str(x).lower())
 
-# Code for part 3
-def create_table(instances):
-    df = pd.DataFrame()
-    df['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(100)]
-    for i in range(3000):
-        df[i] = npr.random(100)
+    # Lemmatise and remove stop
+    lt = LemmaTokenizer()
+    df['Word'] = df['Word'].apply(lambda x: lt.lemmatize(x, True))
+
+    #split tag column into prefix and entities
+    df_tags = df['Tag'].str.split('-', expand=True)
+    df.insert(len(df.columns), 'Tag_prefix', df_tags.iloc[:, 0])
+    df.insert(len(df.columns), 'Tag_entity', df_tags.iloc[:, 1])
+
+    # Drop rows that have been tagged as stop/NAN
+    #df.dropna(inplace=True, subset=["Word"])
+
+    df['Word_seq'] = df.groupby('Sentence #').cumcount()+1
 
     return df
 
-def ttsplit(bigdf):
-    df_train = pd.DataFrame()
-    df_train['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(80)]
-    for i in range(3000):
-        df_train[i] = npr.random(80)
+# Code for part 2
+class sentence:
+    def __init__(self):
+        self.id = None
+        self.entities = []
+        self.sentence_text = ""
+        self.sentence_list = []
+        self.length = 0
 
-    df_test = pd.DataFrame()
-    df_test['class'] = [random.choice(['art','eve','geo','gpe','nat','org','per','tim']) for i in range(20)]
-    for i in range(3000):
-        df_test[i] = npr.random(20)
-        
-    return df_train.drop('class', axis=1).to_numpy(), df_train['class'], df_test.drop('class', axis=1).to_numpy(), df_test['class']
+    def create(self, r):
+        self.sentence_id = r[0]
+        self.sentence_text = r[1][0]
+        self.sentence_list = r[1][1]
+        self.entities = r[1][2]
+
+        self.length = len(self.sentence_list)  # number of entities/words present in the sentence
+        for entity in self.entities:
+            entity.set_parent(self)
+
+    def previous_entities(self, position, n):
+        if position > self.length:
+            ValueError('Invalid parameter specific for position', position)
+
+        return self.entities[:position - 1][-n:]
+
+    def next_entities(self, position, n):
+        if position > self.length:
+            ValueError('Invalid parameter specific for position', position)
+
+        return self.entities[position:][:n]
+
+    def get_features(self, n=5, pad=True,skip_ne=True):
+        VALID_TAGS = ['art', 'eve', 'geo', 'gpe', 'nat', 'org', 'per', 'tim']
+        words = []
+
+        if not isinstance(n, int) or n < 0:
+            ValueError('Invalid parameter specific for n', n)
+
+        # Generate features
+        encode_strings = ["S{}".format(i) for i in range(1, n + 1)]
+
+        sentence_features = []
+        for entity in self.entities:
+
+            # check whether tag is valid
+            if entity.tag_entity in VALID_TAGS:
+                # ---previous---
+                prev_features = [ent.word for ent in self.previous_entities(entity.position, n) if not (ent.isne and skip_ne)]
+
+                # paddings
+                paddings = encode_strings[len(prev_features):]
+                if pad and len(paddings) > 0:
+                    prev_features = paddings + prev_features
+
+                # ---next---
+                next_features = [ent.word for ent in self.next_entities(entity.position, n) if not (ent.isne and skip_ne)]
+
+                # paddings
+                paddings = encode_strings[len(next_features):][::-1]
+                if pad and len(paddings) > 0:
+                    next_features = next_features + paddings
+
+                #concat both prev and next
+                sentence_features.append(
+                    pd.DataFrame.from_dict({entity.tag_entity: prev_features + next_features}, orient='index'))
+
+        # concat all dataframes
+        if sentence_features:
+            return pd.concat(sentence_features)
+        else:
+            return pd.DataFrame()
+
+
+# entity
+class entity:
+    def __init__(self, word, pos, tag_prefix, tag_entity, position):
+        self.word = word
+        self.pos = pos  # part of speech
+        self.tag_prefix = tag_prefix
+        self.tag_entity = tag_entity
+        self.position = position  # position of word in text
+        self.isfirst = (position == 1)
+        self.islast = None
+        self.isne   = not (tag_entity is None)
+    def __str__(self):
+        return self.word
+
+    def set_parent(self, parent):
+        self.parent = parent
+        self.islast = (self.position == parent.length)
+
+
+class gmb_processor:
+    def __init__(self, data):
+        self.data = data
+        self.df_aggregated_sentences = None
+        self.sentences = []
+
+    def __aggregator_fuction(self, s):
+        sentence_list = []
+        sentence_entities = []
+        sentence_text = " "
+
+        # iterate
+        position = 0
+        for (w, p, tp, te) in (zip(s["Word"].values.tolist(),
+                                   s["POS"].values.tolist(),
+                                   s["Tag_prefix"].values.tolist(),
+                                   s["Tag_entity"].values.tolist())):
+
+            if str(w).isalnum():  # ignore punctuation
+                sentence_list.append(w)
+                sentence_entities.append(entity(w, p, tp, te, position + 1))
+                position = position + 1
+
+        sentence_text = " ".join(sentence_list)
+
+        return pd.Series({"sentence_text": sentence_text,
+                          "sentence_list": sentence_list,
+                          "sentence_entities": sentence_entities})
+
+    def __aggregate(self):  # Agg df to sentences
+        ds_result = self.data.groupby("Sentence #").apply(self.__aggregator_fuction)
+        ds_result["Sentence #"] = ds_result.index.astype(float).astype(int)
+
+        return ds_result
+
+    def fit(self):
+        # Agg df to sentences
+        self.df_aggregated_by_sentence_id = self.__aggregate()
+
+        # Convert df to sentence instance
+        for row in self.df_aggregated_by_sentence_id.iterrows():
+            sent = sentence()
+            sent.create(row)
+            self.sentences.append(sent)
+        return self.sentences
+
+    def get_instances(self,n, pad,skip_ne):
+        if (len(self.df_aggregated_by_sentence_id) == 0) or (len(self.sentences) == 0):
+            raise Exception("You need to run fit() first")
+
+        instances = []
+        # iterate over all sentences
+        for sent in self.sentences:
+            instances.append(sent.get_features(n,pad,skip_ne))
+
+        return pd.concat(instances)
+
+def create_instances(data,n=5, pad=True,skip_ne=True):
+    #processor
+    processor = gmb_processor(data)
+
+    #Convert df to sentences
+    sentences = processor.fit()
+
+    #return feature instances as a df
+    df_instances = processor.get_instances(n=n,pad=pad,skip_ne=skip_ne)
+    return df_instances
+
+
+# Code for part 3
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer,TfidfTransformer
+
+def create_table(instances,method='tfidf'):
+    #Convert instances to text documents
+    ds_instance_documents = instances.apply(lambda row: " ".join([elem for elem in row]),axis=1)
+
+    if method=='cv':
+        #Count vectorizer
+        vectorizer = CountVectorizer()
+        result = vectorizer.fit_transform(ds_instance_documents.to_list()).toarray()
+
+        df = pd.DataFrame(data=result, columns=vectorizer.get_feature_names())
+        df.insert(0, '_class_', ds_instance_documents.index)
+
+    elif method=='tfidf':
+        #tfidf
+        #Mindf = 1. Include all tokens that appear at least in one document (all tokens)
+        #Maxdf = 1.0. Include all tokens that appear in ALL documents (100%) (all tokens)
+        tfidf = TfidfVectorizer(ds_instance_documents.to_list())
+        result = tfidf.fit_transform(ds_instance_documents.to_list()).toarray()
+
+        df = pd.DataFrame(data=result, columns=tfidf.get_feature_names())
+        df.insert(0, '_class_', ds_instance_documents.index)
+
+    else:
+        ValueError('Invalid parameter specific for method',method)
+
+
+    return df
+
+def ttsplit(bigdf,test_size=0.2):
+    X = bigdf.drop(columns=['_class_'],axis=1)
+    y = bigdf['_class_']
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size)
+
+    return X_train, y_train.to_numpy(), X_test, y_test.to_numpy()
+
 
 # Code for part 5
 def confusion_matrix(truth, predictions):
-    print("I'm confusing.")
-    return "I'm confused."
+    skplt.metrics.plot_confusion_matrix(test_y, test_predictions, normalize=True)
 
 # Code for bonus part B
 def bonusb(filename):
